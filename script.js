a841736cbfeaf9d3946d943826e1cbc9cceb0939
Maha Abdelnabi <mahaomar@gmail.com>
2023-01-14 10:39:27 -0500

    prompting box

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  //prompting
+ function getPassLen() {
+   var passLength = prompt(
+     "How many characters would you like your password to contain?"
+   );
+
+   while (passLength < 8 || passLength > 128) {
+     passLength = prompt("please eneter number between 8-128");
+   }
+
+   return passLength;
+ }
+
+ function getUpper() {
+   var isUpperCase = confirm("Would you like your password to have UPPERCASE?");
+   return isUpperCase;
+ }
+
+ function getLower() {
+   var isLowerCase = confirm("Would you like your password to have Lowercase?");
+   return isLowerCase;
+ }
+
+ function getNumber() {
+   var isNumber = confirm("Would you like your password to have Numbers?");
+   return isNumber;
+ }
+ function getSymbol() {
+   var isSymbol = confirm("Would you like your password to have Symbols?");
+   return isSymbol;
+ } 
+}
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+passwordText.value = password;
 }
 
 // Add event listener to generate button
